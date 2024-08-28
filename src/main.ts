c57feb3e74b7dbfabea35d401961685856897046
Kili S <79143155+klinshy@users.noreply.github.com>
2024-08-28 14:23:37 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,72 +25,17 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+let helpPopup: any;
+const ctaHelp = [{
+    label: 'Go to hub',
+    className: 'primary',
+    callback: () => WA.nav.openTab('https://world.cocreation.world')
+}];
+WA.room.area.onEnter('needHelp').subscribe, () => {
+helpPopup = WA.ui.openPopup('hopupHelp','Do you wanna know more about us? Explore our Hub!', ctaHelp)} 
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
-let currentZone: string;
-
-const config = [
-    {
-        zone: 'needHelp',
-        message: 'Do you wanna know more about us? Explore our Hub!',
-        cta: [
-            {
-                label: 'go to hub',
-                className: 'primary',
-                callback: () => WA.nav.openTab('https://world.cocreation.world'),
-            }
-        ]
-    },
-    {
-        zone: 'followUs',
-        message: 'Hey! Have you already started following us?',
-        cta: [
-            {
-                label: 'LinkedIn',
-                className: 'primary',
-                callback: () => WA.nav.openTab('https://www.linkedin.com/company/cocreationworld'),
-            },
-            {
-                label: 'Newsletter',
-                className: 'primary',
-                callback: () => WA.nav.openTab('https://cocreation.world'),
-            }
-        ]
-    },
-]
-
-
-WA.onEnterZone('needHelp', () => {
-    currentZone = 'needHelp'
-    openPopup(currentZone, currentZone + 'Popup')
-});
-WA.onEnterZone('followUs', () => {
-    currentZone = 'followUs'
-    openPopup(currentZone, currentZone + 'Popup')
-});
-WA.onLeaveZone('needHelp', closePopup);
-WA.onLeaveZone('followUs', closePopup);
-
-function openPopup(zoneName: string, popupName: string) {
-    const zone = config.find((item) => {
-        return item.zone == zoneName
-    });
-    if (typeof zone !== 'undefined') {
-        // @ts-ignore otherwise we can't use zone.cta object
-        currentPopup = WA.openPopup(popupName, zone.message, zone.cta)
-    }
-}
-function closePopup(){
-    if (typeof currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+WA.room.area.onLeave('needHelp').subscribe(() => {
+    helpPopup.close();
+})
 
 export {};
