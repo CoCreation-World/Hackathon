9b77de901a82bcbc69135e5cc6b26a07158c851c
Kili S <79143155+klinshy@users.noreply.github.com>
2024-08-28 18:39:23 +0200

    final edits today

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
+
 
 console.log('Script started successfully');
 
@@ -18,7 +31,7 @@ WA.onInit().then(() => {
 
 
 let helpPopup: any;
-WA.onInit().then(() => {WA.room.area.onEnter('needHelp').subscribe, () => {
+WA.onInit().then(() => {WA.room.area.onEnter('needHelpPopup').subscribe, () => {
 helpPopup = WA.ui.openPopup('hopupHelp','Do you wanna know more about us? Explore our Hub!',[{
     label: 'Go to hub',
     className: 'primary',
@@ -28,19 +41,6 @@ helpPopup = WA.ui.openPopup('hopupHelp','Do you wanna know more about us? Explor
 WA.room.area.onLeave('needHelp').subscribe(() => {
     helpPopup.close();
 })})
-
-// Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-
-}).catch(e => console.error(e));
 
 
 
