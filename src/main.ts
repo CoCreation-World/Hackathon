12c76025238a33ac0646ab571d184a970a342c24
Kili S <79143155+klinshy@users.noreply.github.com>
2024-09-25 14:56:24 +0200

    allow open Close rooms

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { getLayersMap } from '@workadventure/scripting-api-extra';
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
+    console.log('Player tags: ', WA.player.tags);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -14,39 +14,58 @@ WA.onInit().then(() => {
 
 }).catch(e => console.error(e));
 
-
-console.log('Script started successfully');
+getLayersMap().then(() => {
+    console.log('getLayersMap ready');
+}).catch(e => console.error(e));
 
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ', WA.player.tags);
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
         WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    });
 
+    let helpPopup: any;
+    WA.room.area.onEnter('needHelpPopup').subscribe(() => {
+        helpPopup = WA.ui.openPopup('popupHelp', 'Do you wanna know more about us? Explore our Hub!', [{
+            label: 'Go to hub',
+            className: 'primary',
+            callback: () => WA.nav.openTab('https://world.cocreation.world')
+        }]);
+    });
 
+    WA.room.area.onLeave('needHelp').subscribe(() => {
+        helpPopup.close();
+    });
 
+    function checkAndSetProperty() {
+        getLayersMap().then(() => {
+            const lockAll = WA.state.lockAll;
+            const roomNames: string[] = ['Blue', 'Pink', 'Yellow', 'Red', 'Orange'];
+            roomNames.forEach(roomName => {
+                const propertyName = `doorstep${roomName}In`;
+                if (lockAll === "") {
+                    WA.room.setProperty(propertyName, "code", "");
+                } else {
+                    console.log(`Variable lockAll is not empty for ${propertyName}, no action taken`);
+                }
+            });
+        }).catch(e => console.error(e));
+    }
 
-let helpPopup: any;
-WA.onInit().then(() => {WA.room.area.onEnter('needHelpPopup').subscribe, () => {
-helpPopup = WA.ui.openPopup('hopupHelp','Do you wanna know more about us? Explore our Hub!',[{
-    label: 'Go to hub',
-    className: 'primary',
-    callback: () => WA.nav.openTab('https://world.cocreation.world')
-}] )} 
+    checkAndSetProperty();
+    WA.onInit().then(() => { checkAndSetProperty(); });
+    // Listen for changes to the lockAll variable
+    WA.state.onVariableChange('lockAll').subscribe(() => {
+        console.log('lockAll variable changed');
+        checkAndSetProperty();
+    });
 
-WA.room.area.onLeave('needHelp').subscribe(() => {
-    helpPopup.close();
-})})
-
-
-
-
-})
-export {};
+});
+export {  };
 
 
 
